d5a0f94a36752674b1313eed11667b4580f45101
waqiju <waqiju@gmail.com>
2026-03-15 23:23:25 +0800

    desktop: 更新依赖库

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import { getCurrentWindow } from '@tauri-apps/api/window'
 import { IslandApp } from './IslandApp'
